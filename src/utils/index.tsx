23a7376982f2bac8d7dd8e43ed5d090c6e18559d
Md Shahed Talukder <shahedtalukder51@gmail.com>
2023-06-25 15:43:32 +0600

    simple changes occured

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,15 +1,12 @@
-import { ReactNode } from "react";
-
 export const checkTypeAndRender = (type: any) => {
     console.log(type)
+    // this is portion will be used to check the cell type and render reusable component based on that as input field
 
     return null;
 }
 
 export const checkValidity = (type: any, value: any) => {
-    console.log(type, value)
 
-    console.log(type?.value?.value);
     if((type && type.value && type.value.value !== undefined) && value !== undefined){
         return type.value.value < value ? true : false;
     }
