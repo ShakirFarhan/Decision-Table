dd5e743b52377203766a93b36133efb25cdb55dd
Shakir Farhan <shakirfarhan123@gmail.com>
2023-07-05 22:03:22 +0530

    CSV import Functionality added

---
+++ src/components/layout/FileModal.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Button, Modal } from 'antd';
+import Papa from 'papaparse';
+import { useStore } from '../../store';
+const FileModal: React.FC = () => {
+  const { addCsvImportColumns } = useStore((store) => store);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [columnHeaders, setColumnHeaders] = useState<any[]>([]);
+  const [columnsRows, setColumnRows] = useState<any[]>([]);
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const handleSubmit = () => {
+    addCsvImportColumns(columnHeaders, columnsRows);
+    setIsModalOpen(false);
+  };
+  const handleFile = (e: any) => {
+    Papa.parse(e.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        let columnHeaders: any = [];
+        let columnValues: any = [];
+        results.data.map((data: any) => {
+          columnHeaders.push(Object.keys(data));
+          columnValues.push(Object.values(data));
+          return null;
+        });
+
+        setColumnHeaders(columnHeaders[0]);
+        setColumnRows(columnValues);
+        // setNewColumns([...newColumns, ...results.data]);
+      },
+    });
+  };
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        Import CSV
+      </Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleSubmit}
+        onCancel={handleCancel}
+      >
+        <div>
+          <input onChange={handleFile} type="file" accept=".csv" name="file" />
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default FileModal;
